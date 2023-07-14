54b2ad4e52f12cd4370a68e7845c0b6f005fab9c
sambilokonga <abebawbegna@gmail.com>
2023-07-14 12:36:06 -0700

    updated

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 import os
+import re
 
 if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
@@ -12,10 +13,28 @@ output_file = sys.argv[2]
 if not os.path.exists(input_file):
     print(f"Missing {input_file}", file=sys.stderr)
     sys.exit(1)
-# The actual conversion logic would go here
-# For this example, we'll simply print the input and output filenames
 
-print(f"Input file: {input_file}")
-print(f"Output file: {output_file}")
+# Define a regular expression pattern for matching headings
+heading_pattern = r'^(#+)\s*(.*)$'
+
+# Read the content of the input file
+with open(input_file, 'r') as f:
+    markdown_content = f.readlines()
+
+# Convert markdown headings to HTML headings
+html_content = []
+for line in markdown_content:
+    match = re.match(heading_pattern, line)
+    if match:
+        level = len(match.group(1))
+        heading_text = match.group(2).strip()
+        html_heading = f"<h{level}>{heading_text}</h{level}>"
+        html_content.append(html_heading)
+    else:
+        html_content.append(line)
+
+# Write the HTML content to the output file
+with open(output_file, 'w') as f:
+    f.writelines(html_content)
 
 sys.exit(0)
