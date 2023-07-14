50854c3d9d5db00ba230ca5badb7018550d72521
sambilokonga <abebawbegna@gmail.com>
2023-07-14 12:24:17 -0700

    updated

--- markdown2html.py
+++ markdown2html.py
@@ -21,7 +21,18 @@ if not os.path.exists(input_file):
 # Convert the Markdown file to HTML
 with open(input_file, 'r') as f:
     markdown_content = f.read()
-    html_content = markdown.markdown(markdown_content)
+    html_content = markdown.markdown(
+        markdown_content,
+        extensions=['markdown.extensions.extra']
+    )
+
+# Parse headings and generate HTML
+html_content = html_content.replace('<h1>', '<h1 class="heading">')
+html_content = html_content.replace('<h2>', '<h2 class="heading">')
+html_content = html_content.replace('<h3>', '<h3 class="heading">')
+html_content = html_content.replace('<h4>', '<h4 class="heading">')
+html_content = html_content.replace('<h5>', '<h5 class="heading">')
+html_content = html_content.replace('<h6>', '<h6 class="heading">')
 
 # Write the HTML content to the output file
 with open(output_file, 'w') as f:
@@ -29,3 +40,8 @@ with open(output_file, 'w') as f:
 
 # Print nothing and exit with status 0 (success)
 sys.exit(0)
+In this updated version, I've added the markdown.extensions.extra extension to the markdown.markdown() function call. This extension enables additional Markdown syntax, including support for headers.
+
+After converting the Markdown content to HTML, the script replaces the opening <h1> to <h6> tags with their respective versions but adds a class="heading" attribute. This allows for styling the headings with CSS if desired.
+
+Save the updated script to markdown2html.py, and use it as mentioned before to convert the Markdown file to HTML, now with support for parsing headings.
